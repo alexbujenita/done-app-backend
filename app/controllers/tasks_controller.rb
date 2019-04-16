1dd612c809f4a0c45bd67a11200ba653164adbdc
Alex Bujenita <dekc89@gmail.com>
2019-04-16 16:24:53 +0100

    tasks controller create destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+  def create
+
+    task = Task.new(task_params)
+    if task.save
+      render json: task
+    else
+      render json: { error: 'Bad Request' }, status: 400
+    end
+  end
+
+  def destroy
+    if Task.find(params[:id]).destroy
+      render json: {info: 'Task has been removed'}
+    else
+      render json: { error: 'Bad Request' }, status: 400
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:completed, :content, :end_date, :priority, :user_id)
+  end
+
 end
